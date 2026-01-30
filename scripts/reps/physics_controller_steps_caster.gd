9e762ed96870d99cc9fcdc8ae1aaa4a28ab60fda
Martin <martin.zackrisson@fripost.org>
2026-01-30 10:40:59 +0100

    Remake debug shapes

--- scripts/reps/physics_controller_steps_caster.gd
+++ scripts/reps/physics_controller_steps_caster.gd
@@ -6,12 +6,12 @@ class_name PhysicsControllerStepCaster
     set(value):
         step_direction = value.normalized()
         _sync_cast_origin()
-        
+
 @export var step_distance: float = 0.2:
     set(value):
         step_distance = value
         _sync_cast_origin()
-    
+
 @export var step_height_max: float = 0.35:
     set(value):
         step_height_max = value
@@ -37,9 +37,9 @@ var body: PhysicsBody3D:
             var n: Node3D = get_parent_node_3d()
             while n != null && n is not PhysicsBody3D:
                 n = n.get_parent_node_3d()
-            
+
             body = n
-            
+
         return body
 
 var up_global: Vector3:
@@ -47,7 +47,7 @@ var up_global: Vector3:
         var b: PhysicsBody3D = body
         if b is CharacterBody3D:
             return NodeUtils.translate_to_global_direction((b as CharacterBody3D).up_direction, b).normalized()
-            
+
         return -basis.y
 
 var shape_half_height: float:
@@ -67,34 +67,37 @@ var shape_half_height: float:
 func _ready() -> void:
     enabled = false
     _sync_cast_origin()
-    
+
 func _sync_cast_origin() -> void:
     var b: PhysicsBody3D = body
     if b == null:
         return
-    
-    var up_delta: Vector3 = up_global * (step_height_max + shape_half_height + min_clearing_above)    
+
+    var up_delta: Vector3 = up_global * (step_height_max + shape_half_height + min_clearing_above)
     var dir: Vector3 = (b.global_basis.x * step_direction.x + b.global_basis.z * step_direction.y).normalized()
-    
+
     global_position = body.global_position + up_delta + dir * step_distance
     target_position.y = -(step_height_max + step_down_max + min_clearing_above)
 
 
 enum StepData { POINT, NORMAL }
 
+func display_debug_not_hitting() -> void:
+    if _debug_shape != null:
+        _debug_shape.global_position = global_position
+        var mat: StandardMaterial3D = _debug_shape.get_active_material(0)
+        mat.albedo_color = Color.BLACK
+
 func can_step_up(data: Dictionary[StepData, Vector3] = {}) -> bool:
     force_shapecast_update()
     if !is_colliding():
-        if _debug_shape != null:
-            _debug_shape.hide()
-            
+        display_debug_not_hitting()
         return false
 
     var pt: Vector3 = get_collision_point(0)
     if _debug_shape != null:
         _debug_shape.global_position = pt
-        _debug_shape.show()
-        
+
     if body != null:
         var projection: float = (pt - body.global_position).dot(up_global)
         if projection <= ignore_step_height || projection > step_height_max:
@@ -106,14 +109,14 @@ func can_step_up(data: Dictionary[StepData, Vector3] = {}) -> bool:
                     mat.albedo_color = Color.BLUE
                 else:
                     mat.albedo_color = Color.RED
-    
+
             return false
- 
+
     if _debug_shape != null:
         var mat: StandardMaterial3D = _debug_shape.get_active_material(0)
         mat.albedo_color = Color.WEB_GREEN
-    
+
     data[StepData.POINT] = pt
     data[StepData.NORMAL] = get_collision_normal(0)
-    
+
     return true
--- scripts/reps/physics_gridplayer_controller.gd
+++ scripts/reps/physics_gridplayer_controller.gd
@@ -10,10 +10,10 @@ var cinematic: bool:
         if cinematic && !value && gridless:
             Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
             _captured_pointer_eventer.active = true
-            
+
         elif !cinematic && value && gridless:
-            _captured_pointer_eventer.active = false            
-            
+            _captured_pointer_eventer.active = false
+
         cinematic = value
 
 @export var _camera: Camera3D
@@ -21,14 +21,14 @@ var camera: Camera3D:
     get():
         return _camera
 
-@export var _captured_pointer_eventer: CapturedMouseEventer         
+@export var _captured_pointer_eventer: CapturedMouseEventer
 
 @export var _stepper: PhysicsControllerStepCaster
 
 @export var _forward: ShapeCast3D
 @export var _left: ShapeCast3D
 @export var _right: ShapeCast3D
-@export var _backward: ShapeCast3D 
+@export var _backward: ShapeCast3D
 
 @export_range(0, 1) var _translation_duration: float = 0.3
 @export_range(0, 1) var _rotation_duration: float = 0.25
@@ -41,10 +41,16 @@ var camera: Camera3D:
 @export var _mouse_sensitivity_yaw: float = 1.0
 @export var _mouse_sensistivity_pitch: float = 1.0
 @export var _gridless_camera_near: float = 0.05
-@export var _gridless_camera_fov: float = 70   
+@export var _gridless_camera_fov: float = 70
 
 @export var _camera_transition_time: float = 0.2
 @export var _allow_vertical_movement: bool
+
+@export var _debug_shapes: Array[Node3D]
+@export var _show_debug_shapes: bool = false:
+    set(value):
+        _show_debug_shapes = value
+        _sync_debug_shape_visibilities()
 
 var _gridded_cam_offset: Vector3
 var _gridless_cam_offset: Vector3
@@ -62,12 +68,12 @@ var _translation_pressed: Dictionary[Movement.MovementType, bool]
 var _allow_continious_translation: bool = true
 
 var gridless: bool:
-    set(value): 
+    set(value):
         if gridless != value:
             # _translation_stack.clear()
             # _translation_pressed.clear()
-            velocity = Vector3.ZERO          
-        
+            velocity = Vector3.ZERO
+
             if value:
                 Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
                 _captured_pointer_eventer.active = true
@@ -95,67 +101,72 @@ var gridless: bool:
                 # NOTE: move needs to happend before turn
                 _attempt_translation(Movement.MovementType.NONE, null, Vector3.ZERO)
                 _attempt_turn(0.0)
-                                
+
         gridless = value
 
 func _ready() -> void:
     _gridded_cam_offset = _camera.position
-    
+
     _gridless_cam_offset = _camera.position
     _gridless_cam_offset.x = 0
     _gridless_cam_offset.z = 0
-    
+
     _gridded_cam_fov = _camera.fov
     _gridded_cam_near = _camera.near
-    
+
     __SignalBus.on_physics_player_ready.emit(self)
     _captured_pointer_eventer.active = gridless
-              
+    _sync_debug_shape_visibilities()
+
 func _input(event: InputEvent) -> void:
     var handled: bool = true
     if event.is_echo():
         return
-        
+
     if event.is_action_pressed("crawl_forward"):
         _push_ontop_of_movement_stack(Movement.MovementType.FORWARD)
-        
+
     elif event.is_action_released("crawl_forward"):
         _release_movement(Movement.MovementType.FORWARD)
-        
+
     elif event.is_action_pressed("crawl_strafe_left"):
         _push_ontop_of_movement_stack(Movement.MovementType.STRAFE_LEFT)
 
     elif event.is_action_released("crawl_strafe_left"):
         _release_movement(Movement.MovementType.STRAFE_LEFT)
-        
+
     elif event.is_action_pressed("crawl_strafe_right"):
         _push_ontop_of_movement_stack(Movement.MovementType.STRAFE_RIGHT)
 
     elif event.is_action_released("crawl_strafe_right"):
         _release_movement(Movement.MovementType.STRAFE_RIGHT)
-        
+
     elif event.is_action_pressed("crawl_backward"):
         _push_ontop_of_movement_stack(Movement.MovementType.BACK)
-    
+
     elif event.is_action_released("crawl_backward"):
         _release_movement(Movement.MovementType.BACK)
 
     else:
         handled = false
-        
+
     if handled && !cinematic:
         get_viewport().set_input_as_handled()
-    
+
     if gridless && event is InputEventMouseMotion && !cinematic:
         var mouse: InputEventMouseMotion = event
         rotation.y -= mouse.relative.x * _mouse_sensitivity_yaw
         var new_pitch: float = _camera.rotation.x - mouse.relative.y * _mouse_sensistivity_pitch
         _camera.rotation.x = clampf(new_pitch, -PI * 2/3, PI * 2/3)
- 
+
+func _sync_debug_shape_visibilities() -> void:
+    for shape: Node3D in _debug_shapes:
+        shape.visible = _show_debug_shapes
+
 func _push_ontop_of_movement_stack(movement: Movement.MovementType) -> void:
     if cinematic:
         return
-        
+
     if gridless:
         if !_translation_stack.has(movement):
             _translation_stack.append(movement)
@@ -166,13 +177,13 @@ func _push_ontop_of_movement_stack(movement: Movement.MovementType) -> void:
 func _release_movement(movement: Movement.MovementType) -> void:
     if gridless:
         _translation_stack.erase(movement)
-        
+
     _translation_pressed[movement] = false
-                
+
 func _physics_process(delta: float) -> void:
     if cinematic:
         return
-        
+
     if gridless:
         _gridless_movement(delta)
     else:
@@ -193,39 +204,42 @@ func _gridless_movement(delta: float) -> void:
                 Movement.MovementType.BACK:
                     direction += basis.z
                 _:
-                    push_error("Player %s's movement %s is not a valid translation" % [name, Movement.name(movement)])       
+                    push_error("Player %s's movement %s is not a valid translation" % [name, Movement.name(movement)])
         if direction.length_squared() > 1.0:
             direction = direction.normalized()
-        
-        if direction.length_squared() > 0.0: 
 
-                
+        if direction.length_squared() > 0.0:
+
+
             var v: Vector3 = direction * _gridless_translation_speed
             velocity.x = v.x
             velocity.z = v.z
-    
+
     else:
         var v: Vector3 = velocity.lerp(Vector3.ZERO, _gridless_friction * delta)
         velocity.x = v.x
         velocity.z = v.z
-            
+
     var angle: float = 0.0
     if Input.is_action_pressed("crawl_turn_left"):
         angle = TAU * delta * _gridless_rotation_speed
     elif Input.is_action_pressed("crawl_turn_right"):
         angle = -TAU * delta * _gridless_rotation_speed
-    
+
     if angle != 0.0:
-        basis = transform.rotated(Vector3.UP, angle).basis          
-    
+        basis = transform.rotated(Vector3.UP, angle).basis
+
     if move_and_slide() && direction.length_squared() > 0:
         # Collides with something
         var step_data: Dictionary[PhysicsControllerStepCaster.StepData, Vector3] = {}
         _stepper.step_direction = Vector2(direction.x, direction.z)
-        
-        if _stepper.can_step_up(step_data):    
-            global_position = step_data[PhysicsControllerStepCaster.StepData.POINT]      
-                  
+
+        if _stepper.can_step_up(step_data):
+            global_position = step_data[PhysicsControllerStepCaster.StepData.POINT]
+
+    elif _show_debug_shapes:
+        _stepper.display_debug_not_hitting()
+
 func _gridfull_movement() -> void:
     if !_translation_stack.is_empty():
         var movement: Movement.MovementType = _translation_stack[0]
@@ -240,38 +254,38 @@ func _gridfull_movement() -> void:
                 _attempt_translation(movement, _backward, global_basis.z)
             _:
                 push_error("Player %s's movement %s is not a valid translation" % [name, Movement.name(movement)])
-        
+
     if Input.is_action_just_pressed("crawl_turn_left"):
         _attempt_turn(PI * 0.5)
     elif Input.is_action_just_pressed("crawl_turn_right"):
         _attempt_turn(-PI * 0.5)
-        
-func _attempt_translation(movement: Movement.MovementType, caster: ShapeCast3D, direction: Vector3) -> void:   
+
+func _attempt_translation(movement: Movement.MovementType, caster: ShapeCast3D, direction: Vector3) -> void:
     if _translation_tween && _translation_tween.is_running() || _rotation_tween && _rotation_tween.is_running():
         return
- 
+
     if direction != Vector3.ZERO:
         if !_allow_vertical_movement:
             direction.y = 0
         direction = direction.normalized()
-            
+
     if caster != null && caster.is_colliding():
         _refuse_movement(movement, caster, direction)
         return
-        
+
     var target: Vector3 = (
         builder.get_closest_global_neighbour_position(global_position, CardinalDirections.vector_to_direction(direction))
         if movement != Movement.MovementType.NONE else
         builder.get_closest_global_grid_position(global_position)
     )
-        
+
     # print_debug("Moving %s in direction %s from %s to %s" % [
     #    CardinalDirections.name(CardinalDirections.vector_to_direction(direction.normalized())),
     #    direction,
     #    global_position,
     #    builder.get_closest_global_neighbour_position(global_position, CardinalDirections.vector_to_direction(direction)),
     #])
-    
+
     _translation_tween = create_tween()
     @warning_ignore_start("return_value_discarded")
     _translation_tween.tween_property(self, "global_position", target, _translation_duration)
@@ -283,7 +297,7 @@ func _attempt_translation(movement: Movement.MovementType, caster: ShapeCast3D, 
 func _handle_translation_end(movement: Movement.MovementType) -> void:
     if !_translation_pressed.get(movement, false):
         _translation_stack.erase(movement)
-        
+
 func _refuse_movement(movement: Movement.MovementType, caster: ShapeCast3D, direction: Vector3) -> void:
     print_debug("Refusing movement %s because %s hits %s of %s" % [
         Movement.name(movement),
@@ -293,33 +307,33 @@ func _refuse_movement(movement: Movement.MovementType, caster: ShapeCast3D, dire
     ])
     var target: Vector3 = builder.get_closest_global_neighbour_position(global_position, CardinalDirections.vector_to_direction(direction))
     var pt: Vector3 = caster.get_collision_point(0)
-    
+
     var l: float = global_position.distance_to(pt)
     l = minf(l, global_position.distance_to(target))
     if movement == Movement.MovementType.FORWARD:
         l *= _refuse_distance_forward
     else:
         l *= _refuse_distance_other
-    
+
     var mid: Vector3 = global_position + l * (target - global_position)
-    
+
     _translation_tween = create_tween()
     @warning_ignore_start("return_value_discarded")
     _translation_tween.tween_property(self, "global_position", mid, _translation_duration * 0.5)
     _translation_tween.tween_property(self, "global_position", global_position, _translation_duration * 0.5)
     @warning_ignore_restore("return_value_discarded")
-    
+
     if _translation_tween.finished.connect(_handle_translation_end.bind(movement)) != OK:
         push_error("Failed to connect to translation tween end")
         _handle_translation_end(movement)
-    
+
 func _attempt_turn(angle: float) -> void:
     if  _rotation_tween && _rotation_tween.is_running():
         return
-    
+
     var t: Transform3D = global_transform.rotated(Vector3.UP, angle)
     var target_global_rotation: Quaternion = builder.get_cardial_rotation(t.basis.get_rotation_quaternion())
-    
+
     _rotation_tween = create_tween()
     @warning_ignore_start("return_value_discarded")
     var tween_func: Callable = QuaternionUtils.create_tween_rotation_method(self)
@@ -330,7 +344,7 @@ static func find_player_in_tree(body: Node3D) -> PhysicsGridPlayerController:
     while body != null:
         if body is PhysicsGridPlayerController:
             return body as PhysicsGridPlayerController
-    
+
         body = body.get_parent_node_3d()
-        
+
     return null
