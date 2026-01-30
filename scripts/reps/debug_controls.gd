4d29e7ed84f84924788384ca83cac40302968469
Martin <martin.zackrisson@fripost.org>
2026-01-30 11:23:58 +0100

    Make a debug scene

---
+++ scripts/reps/debug_controls.gd
@@ -0,0 +1,39 @@
+extends CanvasLayer
+
+@export var _player: PhysicsGridPlayerController
+@export var _cinematic_btn: Button
+@export var _gridless_btn: Button
+@export var _debug_shapes_btn: Button
+
+func _ready() -> void:
+    _sync_cinematic.call_deferred()
+    _sync_gridless.call_deferred()
+    _sync_debug_shapes.call_deferred()
+
+func _input(event: InputEvent) -> void:
+    if event is InputEventKey && !event.is_echo():
+        var e_key: InputEventKey = event
+        if e_key.is_pressed() && e_key.keycode == KEY_ESCAPE:
+            _player.gridless = false
+            _sync_gridless()
+
+func _sync_cinematic() -> void:
+    _cinematic_btn.button_pressed = _player.cinematic
+
+func _sync_gridless() -> void:
+    _gridless_btn.button_pressed = _player.gridless
+
+func _sync_debug_shapes() -> void:
+    _debug_shapes_btn.button_pressed = _player._show_debug_shapes
+
+func _on_cinematic_btn_pressed() -> void:
+    _player.cinematic = !_player.cinematic
+    _sync_cinematic()
+
+func _on_gridless_btn_pressed() -> void:
+    _player.gridless = !_player.gridless
+    _sync_gridless()
+
+func _on_debug_shapes_btn_pressed() -> void:
+    _player._show_debug_shapes = !_player._show_debug_shapes
+    _sync_debug_shapes()
--- scripts/reps/dungeon_builder.gd
+++ scripts/reps/dungeon_builder.gd
@@ -19,13 +19,15 @@ func _enter_tree() -> void:
         push_error("Failed to connect complete dungeon plan")
     if __SignalBus.on_use_pickax.connect(_handle_use_pickax) != OK:
         push_error("Failed to connect use pickax")
-        
+
 func _exit_tree() -> void:
     __SignalBus.on_complete_dungeon_plan.disconnect(_handle_complete_dungeon_plan)
     __SignalBus.on_use_pickax.disconnect(_handle_use_pickax)
-    
+
 func _ready() -> void:
     player.cinematic = true
+    player.global_position = get_closest_global_grid_position(player.global_position)
+    player.builder = self
 
 func _handle_use_pickax(target: Node3D, _hack_direction: CardinalDirections.CardinalDirection, point: Vector3) -> void:
     while !target.has_meta(_COORDINATES_META):
@@ -39,7 +41,7 @@ func _handle_use_pickax(target: Node3D, _hack_direction: CardinalDirections.Card
             coords, !dirts.has(coords), dirts[coords], target,
         ])
         return
-    
+
     var origin: Vector3 = get_global_grid_position_from_coordinates(coords)
     var side_points: Array[Vector3] = [
         origin + CardinalDirections.direction_to_vector(CardinalDirections.CardinalDirection.SOUTH) * grid_size * 0.5,
@@ -65,7 +67,7 @@ func _handle_use_pickax(target: Node3D, _hack_direction: CardinalDirections.Card
     if closest_idx < 0:
         push_error("Failed to figure out side to dig out!")
         return
-        
+
     var digout_direction: CardinalDirections.CardinalDirection = side_directions[closest_idx]
     print_debug("Digging %s" % [CardinalDirections.name(digout_direction)])
     _digout_coords(coords, digout_direction, target)
@@ -74,7 +76,7 @@ func _handle_use_pickax(target: Node3D, _hack_direction: CardinalDirections.Card
         _digout_coords(neighbour, CardinalDirections.invert(digout_direction), dirts[neighbour])
 
 func _digout_coords(coords: Vector3i, digout_direction: CardinalDirections.CardinalDirection, target: Node3D) -> void:
-    var digs: Array[CardinalDirections.CardinalDirection] = []    
+    var digs: Array[CardinalDirections.CardinalDirection] = []
     if !dirt_digouts.has(coords):
         digs = [digout_direction]
         dirt_digouts[coords] = digs
@@ -83,7 +85,7 @@ func _digout_coords(coords: Vector3i, digout_direction: CardinalDirections.Cardi
         digs = dirt_digouts[coords]
     else:
         digs = dirt_digouts[coords]
-    
+
     print_debug("Placing new dirt at %s with digs %s" % [coords, digs.map(func (c: CardinalDirections.CardinalDirection) -> String: return CardinalDirections.name(c))])
     var new_dirt: Node3D = _place_dirt(coords, digs)
     if new_dirt != null:
@@ -93,96 +95,95 @@ func _digout_coords(coords: Vector3i, digout_direction: CardinalDirections.Cardi
         print_debug("Didn't get any new dirt!")
 
 var first_room: bool = true
-    
+
 func _handle_complete_dungeon_plan(elevation: int, rooms: Array[BlueprintRoom]) -> void:
     var grid: Grid2D = null
     var exposed_dirt: bool = false
-    
+
     for room: BlueprintRoom in rooms:
         if room.option == null:
             push_error("Bluepint Room %s lacks an option, no clue what room to place" % room)
             continue
-        
+
         if placed_rooms.any(func (pr: Node3D) -> bool: return pr.get_meta(_BLUEPRINT_META) == room):
-            continue 
-            
+            continue
+
         if grid == null:
             grid = room.grid
-        
+
         if !exposed_dirt && room.has_unused_door():
             exposed_dirt = true
-                    
+
         var room_3d: Node3D = room.option.instantiate_3d_room()
         placed_rooms.append(room_3d)
         room_3d.set_meta(_BLUEPRINT_META, room)
         add_child(room_3d)
-        
+
         room_3d.rotation = CardinalDirections.direction_to_rotation(
-            CardinalDirections.CardinalDirection.UP, 
+            CardinalDirections.CardinalDirection.UP,
             room.get_rotation_direction(),
         ).get_euler()
-        
+
         var room_tiles: Array[Vector3i] = Array(
             room.get_global_used_tiles().map(func (v2: Vector2i) -> Vector3i: return Vector3i(v2.x, elevation, v2.y)),
             TYPE_VECTOR3I,
             "",
             null,
         )
-   
+
         for room_tile: Vector3i in room_tiles:
             if dirts.has(room_tile):
                 dirts[room_tile].queue_free()
                 if !dirts.erase(room_tile):
                     push_warning("Couldn't fully clear dirt")
-                
+
                 if dirt_digouts.erase(room_tile):
                     push_warning("Couldn't fully clear dirt digout data")
-                
+
                 # We'll readd it below, just dont want dupes
                 used_tiles.erase(room_tile)
-                    
+
         used_tiles.append_array(room_tiles)
-        
+
         var origin2d: Vector2i = room.get_origin()
         var origin: Vector3i = Vector3i(origin2d.x, elevation, origin2d.y)
-        
+
         room_3d.global_position = get_global_grid_position_from_coordinates(origin)
         # print_debug("Placed room %s at %s %s with tiles %s" % [room, room_3d.position, origin, room_tiles])
 
         room_3d.set_meta(_ORIGIN_META, origin)
-        
+
         if first_room && player != null:
             var ppos: Vector2i = room.get_global_used_tiles()[0]
             player.global_position = get_global_grid_position_from_2d_coordinates(ppos, elevation)
-            player.builder = self
             first_room = false
             print_debug("Player located at %s at the center of %s" % [player.global_position, ppos])
-    
+
     if exposed_dirt:
         _populate_level_with_dirt(grid, elevation)
-                    
-                        
+
+
     player.cinematic = false
 
 func _populate_level_with_dirt(grid: Grid2D, elevation: int) -> void:
     if grid != null:
         for x: int in range(grid.extent.position.x, grid.extent.end.x):
             for y: int in range(grid.extent.position.y, grid.extent.end.y):
-                var coords3d: Vector3i = Vector3i(x, elevation, y)                
+                var coords3d: Vector3i = Vector3i(x, elevation, y)
                 if used_tiles.has(coords3d):
                     continue
-                    
+
 
                 if _place_dirt(coords3d) == null:
                     push_warning("%s: Failed to place dirt at %s" % [name, coords3d])
                 else:
                     used_tiles.append(coords3d)
 
-## Get the coordinates closest to the global position    
+## Get the coordinates closest to the global position
 func get_closest_coordinates(global_pos: Vector3) -> Vector3i:
     var local: Vector3 = to_local(global_pos)
-    return Vector3i(roundi(local.x / grid_size.x), roundi(local.y / grid_size.y), roundi(local.z / grid_size.z))   
-           
+    return Vector3i(roundi(local.x / grid_size.x), roundi(local.y / grid_size.y), roundi(local.z / grid_size.z))
+
 func _place_dirt(coords: Vector3i, digs: Array[CardinalDirections.CardinalDirection] = []) -> Node3D:
     var global_pos: Vector3 = get_global_grid_position_from_coordinates(coords)
     var d: Node3D = dirt_mag.place_block_at(self, global_pos, digs)
@@ -197,21 +198,21 @@ func _place_dirt(coords: Vector3i, digs: Array[CardinalDirections.CardinalDirect
 
 func get_global_grid_position_from_2d_coordinates(coords: Vector2i, elevation: int) -> Vector3:
     return Vector3(coords.x * grid_size.x, elevation * grid_size.y, coords.y * grid_size.z)
-    
+
 func get_global_grid_position_from_coordinates(coords: Vector3i) -> Vector3:
     return Vector3(coords.x * grid_size.x, coords.y * grid_size.y, coords.z * grid_size.z)
-    
+
 func _get_closest_local_grid_position(global_pos: Vector3) -> Vector3:
     var local: Vector3 = to_local(global_pos)
     return Vector3(roundi(local.x / grid_size.x) * grid_size.x, roundi(local.y / grid_size.y) * grid_size.y, roundi(local.z / grid_size.z) * grid_size.z)
-    
+
 func get_closest_global_grid_position(global_pos: Vector3) -> Vector3:
     return to_global(_get_closest_local_grid_position(global_pos))
 
 # Get closest neighbour to a global position in direction of the grid
 func get_closest_global_neighbour_position(global_pos: Vector3, direction: CardinalDirections.CardinalDirection) -> Vector3:
     return to_global(_get_closest_local_grid_position(global_pos) + CardinalDirections.direction_to_vector(direction) * grid_size)
-    
+
 func get_2d_grid_float_position(global_pos: Vector3) -> Vector2:
     var pos: Vector3 = to_local(global_pos) / grid_size
     return Vector2(pos.x, pos.z)
@@ -230,7 +231,7 @@ static func find_builder_in_tree(body: Node3D) -> DungeonBuilder:
     while body != null:
         if body is DungeonBuilder:
             return body as DungeonBuilder
-    
+
         body = body.get_parent_node_3d()
-        
+
     return null
