89355803a7744574c9ffd12af76aa57e2bb6bfa6
Martin <martin.zackrisson@fripost.org>
2026-02-02 14:55:51 +0100

    Fix collisions in one room

--- scripts/reps/dungeon_planner_ui.gd
+++ scripts/reps/dungeon_planner_ui.gd
@@ -16,7 +16,7 @@ func _enter_tree() -> void:
 func _exit_tree() -> void:
     __SignalBus.on_update_planning.disconnect(_handle_update_planner)
     __SignalBus.on_elevation_plan_sealed.disconnect(_handle_elevation_plan_sealed)
-    
+
 func _on_layout_done_btn_pressed() -> void:
     _planner.complete_planning()
 
@@ -27,17 +27,17 @@ func _handle_elevation_plan_sealed(elevation: int) -> void:
         _sealed_elevations.append(elevation)
 
     message_label.text = tr(_SEALED_ELEVATION_MESSAGE_ID)
-    
+
 func _handle_update_planner(planner: DungeonPlanner, remaining: int) -> void:
     _planner = planner
     if _sealed_elevations.has(planner.elevation):
         message_label.text = tr(_SEALED_ELEVATION_MESSAGE_ID)
     else:
         message_label.text = ""
-        
+
     if counter_label == null:
         return
-    
+
     counter_label.text = tr("UI_REMAINING_COUNT").format({"remaining_count": remaining})
     redraw_btn.disabled = !planner.can_redraw_rooms
     redraw_btn.text = tr("UI_REDRAW_ROOMS").format({"cost": planner.redraw_cost})
